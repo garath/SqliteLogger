0c91efbca8b4ee4027831319379584454dd6492c
Michael Stuckey <michael.stuckey@gmail.com>
2021-07-25 17:34:46 -0700

    WIP

---
+++ ConsoleStress/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using SqliteLogger;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleStress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(loggingConfiguration =>
+                {
+                    loggingConfiguration.AddSqliteLogger(config => );
+                })
+                .Build();
+        }
+    }
+
+    internal class LoggingStressWorker : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
